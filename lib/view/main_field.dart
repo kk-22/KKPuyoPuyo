b8ba84040c02945f52ce88b677d744e21ad63e65
kk-22 <85630292+kk-22@users.noreply.github.com>
2021-11-04 09:15:49 +0900

    初期コード削除

---
+++ lib/view/main_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainField extends StatelessWidget {
+  const MainField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
